b362dbafb2aa70dafb5f5f30e5b6558d93fc364d
user <kiran9726@gmail.com>
2023-08-24 17:54:49 +0530

    readvalue added

--- readvalue.sh
+++ readvalue.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "enter value one"
-read -i  VAL1
+read -s VAL1
 echo "enter value two"
-read  VAL2
+read  -s VAL2
 echo "math operations"
 echo "addition of $VAL1 and  $VAL2 = $(($VAL1 + $VAL2))"
 echo "subtraction of $VAL1 and  $VAL2 = $(($VAL1 - $VAL2))"
