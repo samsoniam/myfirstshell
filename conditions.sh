92ffea89ff9c8aba339bfe5e371ecda69510fc00
user <kiran9726@gmail.com>
2023-08-24 18:55:28 +0530

    readvalue added

--- conditions.sh
+++ conditions.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 myinput=$1
-if[$myinput -gt 10]
+if [ $myinput -gt 10 ]
 then
-echo "$myinput is greater than 10"
+   echo "$myinput is greater than 10"
 else
-echo "$myinput is less than 10"
+   echo "$myinput is less than 10"
 fi
