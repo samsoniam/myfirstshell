098ff37eae9bb30fab93444783e8a410bb5ffcfa
user <kiran9726@gmail.com>
2023-08-24 18:50:53 +0530

    readvalue added

--- conditions.sh
+++ conditions.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-userid=$(id -u)
-if[$userid !=0]
+# userid=$(id -u)
+# if[$userid !=0]
+# then
+# echo "user is not root user"
+# else
+# echo "user is root user"
+# fi
+myinput=$1
+if[myinput -gt 10]
 then
-echo "user is not root user"
+echo "$myinput is greater than 10"
 else
-echo "user is root user"
+echo "$myinput is less than 10"
 fi
