36794e212d4488035992bca6b94328cb7d139c34
user <kiran9726@gmail.com>
2023-08-24 15:52:24 +0530

    matop added

--- mathops.sh
+++ mathops.sh
@@ -2,7 +2,7 @@
 VAL1=$1
 VAL2=$2
 echo "math operations"
-echo "addition of $VAL1 and  $VAL2 = ${$VAL1+$VAL2}"
-echo "subtraction of $VAL1 and  $VAL2 = ${$VAL1-$VAL2}"
-echo "multi of $VAL1 and  $VAL2 = ${$VAL1*$VAL2}"
-echo "division of $VAL1 and  $VAL2 = ${$VAL1/$VAL2}"
+echo "addition of $VAL1 and  $VAL2 = {$VAL1+$VAL2}"
+echo "subtraction of $VAL1 and  $VAL2 = {$VAL1-$VAL2}"
+echo "multi of $VAL1 and  $VAL2 = {$VAL1*$VAL2}"
+echo "division of $VAL1 and  $VAL2 = {$VAL1/$VAL2}"
