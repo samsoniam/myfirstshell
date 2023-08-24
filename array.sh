308734b78b0ae5dd51840e4da6bde75487b31402
user <kiran9726@gmail.com>
2023-08-24 18:09:53 +0530

    readvalue added

--- array.sh
+++ array.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "array in shell script "
 PERSONS=("RAHUL","RAVI","SANJU")
-echo "array items are $PERSONS[0] and $PERSONS[1] "
+echo "array items are ${PERSONS[0]} and ${PERSONS[1]} "
