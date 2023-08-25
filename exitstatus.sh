d005ea237bd1c4e8b839c229219b7fdab051aacf
user <kiran9726@gmail.com>
2023-08-25 21:58:14 +0530

    exit status added

--- exitstatus.sh
+++ exitstatus.sh
@@ -5,11 +5,24 @@ if [ $USERID -ne 0 ]
 then
 echo "user is not root user"
 exit 1
+
 fi
 yum install mysql -y
 if [ $? -ne 0]
 then
   echo "install fail"
+  exit 1
 else
   echo "install success"
+
+  fi
+
+yum install postfix -y
+if [ $? -ne 0]
+then
+  echo "install postfix fail"
+  exit 1
+else
+  echo "install postfix success"
+
   fi
